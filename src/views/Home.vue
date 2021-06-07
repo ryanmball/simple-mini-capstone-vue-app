d1bf70d82f98857dd715647be8957989d06fd131
ryan <ball.ryan.rb@gmail.com>
2021-06-07 11:33:18 -0600

    adds productUpdate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,20 @@
   <div class="home">
     <div>
       <h3>Create new product:</h3>
-      <input type="text" v-model="newProductName" placeholder="name"><br>
-      <input type="text" placeholder="description" v-model="newProductDescription"><br>
-      <input type="text" placeholder="price" v-model="newProductPrice"><br>
-      <input type="text" placeholder="image url" v-model="newProductImageUrl"><br>
-      <button v-on:click="productCreate()" >Add Product</button>
-      <p v-if="errors">ERRORS:<br>{{ errors }}</p>
+      <input type="text" v-model="newProductName" placeholder="name" /><br />
+      <input
+        type="text"
+        placeholder="description"
+        v-model="newProductDescription"
+      /><br />
+      <input type="text" placeholder="price" v-model="newProductPrice" /><br />
+      <input
+        type="text"
+        placeholder="image url"
+        v-model="newProductImageUrl"
+      /><br />
+      <button v-on:click="productCreate()">Add Product</button>
+      <p v-if="errors">ERRORS:<br />{{ errors }}</p>
     </div>
     <h1><u>Products</u></h1>
     <div v-for="product in products" v-bind:key="product.id">
@@ -22,9 +30,16 @@
       <form method="dialog">
         <h1>Product Info</h1>
         <img :src="currentProduct.image_url" alt="" />
-        <p>Title: {{ currentProduct.name }}</p>
-        <p>Description: {{ currentProduct.description }}</p>
-        <p>Price: {{ currentProduct.price }}</p>
+        <p>Name: <input type="text" v-model="currentProduct.name" /></p>
+        <p>
+          Description:
+          <input type="text" v-model="currentProduct.description" />
+        </p>
+        <p>Price: <input type="text" v-model="currentProduct.price" /></p>
+        <p>
+          Image Url: <input type="text" v-model="currentProduct.image_url" />
+        </p>
+        <button v-on:click="productUpdate()">Update</button>
         <button>Close</button>
       </form>
     </dialog>
@@ -46,7 +61,7 @@ export default {
       newProductPrice: "",
       newProductImageUrl: "",
       errors: false,
-      currentProduct: ""
+      currentProduct: "",
     };
   },
   created: function () {
@@ -54,7 +69,7 @@ export default {
   },
   methods: {
     productIndex: function () {
-      axios.get("http://localhost:3000/products").then(response => {
+      axios.get("http://localhost:3000/products").then((response) => {
         console.log(response.data);
         this.products = response.data;
       });
@@ -66,14 +81,16 @@ export default {
         price: this.newProductPrice,
         image_url: this.newProductImageUrl,
       };
-      axios.post("http://localhost:3000/products", params).then(response => {
-        console.log("Product added!", response.data);
-        this.products.push(response.data);
-      })
-      .catch(error => {
-        console.log(error.response.data.errors);
-        this.errors = error.response.data.errors;
-      })
+      axios
+        .post("http://localhost:3000/products", params)
+        .then((response) => {
+          console.log("Product added!", response.data);
+          this.products.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
       this.newProductName = "";
       this.newProductDescription = "";
       this.newProductPrice = "";
@@ -83,7 +100,28 @@ export default {
       console.log(product);
       this.currentProduct = product;
       document.querySelector("#product-details").showModal();
-    } 
+    },
+    productUpdate: function () {
+      console.log(this.currentProduct);
+      var productUpdateParams = {
+        name: this.currentProduct.name,
+        description: this.currentProduct.description,
+        price: this.currentProduct.price,
+        image_url: this.currentProduct.image_url,
+      };
+      axios
+        .patch(
+          `http://localhost:3000/products/${this.currentProduct.id}`,
+          productUpdateParams
+        )
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
+    },
   },
 };
 </script>
